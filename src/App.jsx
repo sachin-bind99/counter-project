81abd22312b74a4d37ee4ef0a37f8552e68b7f3d
Sachin Bind <sachinkumar7307376@gmail.com>
2026-06-11 22:47:45 +0530

    updated counter project

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,15 @@ function App() {
     setCount(count-1)
   }
   return (
-  
-    <div className="flex flex-col items-center justify-center min-h-screen gap-2">
-      <h2 className='text-white'>hello Sachin</h2>
-      <h2 className='text-white'>Counter :{count}</h2>
+    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center gap-4 p-6 bg-gray-800 shadow-cyan-500 border rounded text-white shadow-2xl">
+      <h2 className='text-white '>hello Sachin</h2>
+      <h2 className='text-white '>Counter :{count}</h2>
        <div className="flex flex-col items-center gap-4">
       <button onClick={Adding} className="w-48 bg-blue-500 text-white px-4 py-2 rounded">Add Value : {count}</button>
       <button onClick={Removing} className='w-48 bg-red-500 text-white px-4 py-2 rounded'>Remove Value: {count}</button>
       </div>
+    </div>
     </div>
     
   )
